bdac2f4867cba0da6324c73f110f3dbdb8355da0
mateospencer <matt@mattspencer.net>
2020-04-08 22:28:40 +1200

    added 5-2 instructions

--- p78-conditional_tests.py
+++ p78-conditional_tests.py
@@ -46,3 +46,13 @@ print(daughter_age < 1 or son_age < 1)
 
 
 #	Section 5-2: More Conditional Tests
+#	Try to have at least one test (from 5-1 or below) that returns true and one that returns
+#	false for  each of the following:
+#
+#		Tests for equality and inequality with strings
+#		Tests using the lower() method
+#		Numerical tests involving equality and inequality, grater than and less than, greater than 
+#			or equal to, and less than or equal to
+#		Tests using the and keyword and the or keyword
+#		Test whether an item is in a list
+#		Test whether an item is not in a list
