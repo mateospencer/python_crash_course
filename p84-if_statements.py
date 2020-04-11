35fe044b152efa6c3a7f1e300a6f9ef41e334d7d
mateospencer <matt@mattspencer.net>
2020-04-11 22:33:17 +1200

    starting if statements

---
+++ p84-if_statements.py
@@ -0,0 +1,12 @@
+#	Python Crash Course 2nd Edition
+#	Page 84 (If Statements)
+
+#	Section 5-3: Alien Colors #1
+#	* Create a variable called alien_color and assigned it a value of green, yellow, and red
+#	* Write an if statement to test whether the alien's color is green. IF so, print a msg that the
+#		player just earned 5 points. 
+
+alien_color = ['green', 'yellow', 'red']
+
+if 'green' in alien_color:
+	print("You just earned 5 points.")
