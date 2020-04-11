42ec21abe8df3f541f446a80c34a215082eaa59c
mateospencer <matt@mattspencer.net>
2020-04-11 22:49:38 +1200

    problem with else statement in 5-4

--- p84-if_statements.py
+++ p84-if_statements.py
@@ -2,7 +2,7 @@
 #	Page 84 (If Statements)
 
 #	Section 5-3: Alien Colors #1
-#	* Create a variable called alien_color and assigned it a value of green, yellow, and red
+#	Create a variable called alien_color and assigned it a value of green, yellow, and red
 #	* Write an if statement to test whether the alien's color is green. IF so, print a msg that the
 #		player just earned 5 points. 
 
@@ -21,3 +21,26 @@ if 'red' in alien_color:
 
 if 'yellow' in alien_color:
 	prnt("You just earned 20 points.")
+
+#	Section 5-4: Alient Colors #2
+#	Choose a color for an alien as you did above and write an if-else chain. 
+#	* If the alien's color is green, print that the player just earned 5 points for shooting
+#		the alien. 
+#	* If the alien's color isnt green, print that the player just earned 10 points. 
+#	* Write one version of this program that runs the if block and another that runs the else block. 
+
+#	Runs the if block
+alien_color = ['green']
+
+if 'green' in alien_color:
+	print("You just earned 5 points for shooting the alien."
+else: 
+	print("You just earned 10 points.")
+
+#	Runs the else block
+alien_color = ['yellow']
+
+if 'green' in alien_color:
+	print("You just earned 5 points for shooting the alien."
+else: 
+	print("You just earned 10 points.")
