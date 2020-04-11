7b92a52cfa418607a5e07fcb3ddb1b438478320a
mateospencer <matt@mattspencer.net>
2020-04-11 22:38:14 +1200

    more if statements.

--- p84-if_statements.py
+++ p84-if_statements.py
@@ -6,7 +6,18 @@
 #	* Write an if statement to test whether the alien's color is green. IF so, print a msg that the
 #		player just earned 5 points. 
 
-alien_color = ['green', 'yellow', 'red']
+alien_color = ['green']
 
 if 'green' in alien_color:
 	print("You just earned 5 points.")
+
+#	* Write a version of this program that passes the if test and another that fails (fails should
+#	have no output.)
+
+alien_color = ['red']
+
+if 'red' in alien_color:
+	print("You just earned 10 points.")
+
+if 'yellow' in alien_color:
+	prnt("You just earned 20 points.")
