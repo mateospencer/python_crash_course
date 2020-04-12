79c16da81a5fbc9a4ca19a30873255df4ea7819d
mateospencer <matt@mattspencer.net>
2020-04-12 22:37:03 +1200

    beginning 5-7 solution.

--- p84-if_statements.py
+++ p84-if_statements.py
@@ -22,6 +22,7 @@ if 'red' in alien_color:
 if 'yellow' in alien_color:
 	print("You just earned 20 points.")
 
+
 #	Section 5-4: Alien Colors #2
 #	Choose a color for an alien as you did above and write an if-else chain. 
 #	* If the alien's color is green, print that the player just earned 5 points for shooting
@@ -45,6 +46,7 @@ if 'green' in alien_color:
 	print("You just earned 5 points for shooting the alien.")
 else: 
 	print("You just earned 10 points.")
+
 
 #	Section 5-5: Alien Colors #3
 #	Turn if-else chain from 5-4 into an if-elif-else chain.
@@ -77,6 +79,7 @@ elif 'yellow' in alien_color:
 else: 
 	print("You just earned 15 points.")	
 
+
 #	Section 5-6: Stages of Life
 #	Write an if-elif-else chain that determines a person's stage of life. Set a value for the 
 #		variable age, and then:
@@ -102,3 +105,14 @@ elif age >=20 and age <65:
 	print("This person is an adult.")
 else:
 	print("This person is an elder.")
+
+
+#	Section 5-7: Favorite Fruit
+#	Make list of favorite fruits and then write series of indepedent if statements that check for 
+#	certain fruits in list. 
+#	* Make list of three favorite fruits and call it favorite_fruits
+#	* Write five if statements. Each should check whether a certain kind of fruit is in your list.
+#		If the fruit is in your list, the if block should print a statement, such as "You really
+#		like bananas!"
+
+favorite_fruits = ['mango', 'feijoa', 'kiwi']
