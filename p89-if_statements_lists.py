23e68bc1802c10e81db203356a93abff5841fb0a
mateospencer <matt@mattspencer.net>
2020-04-13 21:40:29 +1200

    working solution added for 5-9.

--- p89-if_statements_lists.py
+++ p89-if_statements_lists.py
@@ -19,3 +19,33 @@ for username in usernames:
 		print("Hello admin, would you like to see a status report?")
 	else:
 		print("Hello " + username + ", thank you for logging in again.")
+
+
+#	Section 5-9: No Users
+#	Add an if test to hello_admin.py to make sure the list of users is not empty. 
+#	* If the list is empty, print the message 'We need to find some users!'
+#	* Remove all of the usernames from your list, and make sure the correct message is printed. 
+
+#	With usernames
+usernames = ['admin', 'gtan', 'ksmith', 'lmoyo', 'mdejong', 'nchen', 'odelacruz']
+
+if usernames: 
+	for username in usernames:
+		if username == 'admin':
+			print("Hello admin, would you like to see a status report?")
+		else:
+			print("Hello " + username + ", thank you for logging in again.")
+else:
+	print("We need to find some users!")
+
+#	Without usernames
+usernames = []
+
+if usernames: 
+	for username in usernames:
+		if username == 'admin':
+			print("Hello admin, would you like to see a status report?")
+		else:
+			print("Hello " + username + ", thank you for logging in again.")
+else:
+	print("We need to find some users!")
