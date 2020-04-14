c7bdf6413a8f55a9f2a2fa168228fd75e84cc320
mateospencer <matt@mattspencer.net>
2020-04-15 10:06:08 +1200

    updated 3-7 exercise.

--- p42-changing_list_elements.py
+++ p42-changing_list_elements.py
@@ -78,6 +78,18 @@ for guest in guests:
 
 
 #	Section 3-7: Shrinking Guest List
+#	You just found out that your new dinner table won't arrive in time for the dinner, and you have
+#		space for only two guests. 
+#	* Start with your program from 3-6. Add a new line that prints a message saying that you can
+#		invite only two people for dinner. 
+#	* Use pop() to remove guests from your list one at a time until only two names remain in your
+#		list. Each time you pop a name from your list, print a message to that person letting them
+#		know you're sorry you can't invite them to dinner. 
+#	* Print a message to each of the two people still on your list, letting them know they're still
+#		invited.
+#	* Use del to remove the last two names from your list, so you have an empty list. Print your
+#		list to make sure you actually have an empty list at the end of your program. 
+
 oops_message = "This is embarrassing to admit but our new table hasn't arrived yet and we will only have space for two guests. We'll add you to the top of the list for next time."
 
 # Checking the guests before popping out
