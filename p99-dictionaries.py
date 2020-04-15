8e68bbe7af21cea4ad02350dc12bcd87b2eae57b
mateospencer <matt@mattspencer.net>
2020-04-16 09:49:16 +1200

    6-1 solution added.

---
+++ p99-dictionaries.py
@@ -0,0 +1,14 @@
+#	Python Crash Course 2nd Edition
+#	Page 99 (Dictionaries)
+
+#	Section 6-1: Person
+#	Use a dictionary to store information about a person you know. Store their first name, last
+#		name, age, and the city in which tye live. You should have keys such as first_name, 
+#		last_name, age, and city. Print each piece of information stored in your dictionary
+
+person_0 = {'first_name': 'Jacinda', 'last_name': 'Ardern', 'age': '39', 'city': 'Wellington'}
+
+print(person_0['first_name'])
+print(person_0['last_name'])
+print(person_0['age'])
+print(person_0['city'])
