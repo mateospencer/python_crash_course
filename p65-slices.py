74a42a037607510344ec2c07db692adb111efc5c
mateospencer <matt@mattspencer.net>
2020-04-20 12:31:59 +1200

    consolidating slice_copy here.

--- p65-slices.py
+++ p65-slices.py
@@ -29,3 +29,41 @@ for bird in birds[1:4]:
 print("The last three items in this list are:")
 for bird in birds[-3:]:
 	print(bird.title())
+
+#	Section 4-11: My Pizzas, Your Pizzas
+#	Start with your program from Exercise 4-1 (page 56). Make a copy of the list of pizzas, and call
+#	it friend_pizzas. Then, do the following:
+#	*	Add a new pizza to the original list.
+#	*	Add a different pizza to the list friend_pizzas.
+#	*	Prove that you have two separate lists. Print the message My favorite pizzas are:, and then
+#		use a for loop to print the first list. Print the message My friend's favorite pizzas are:,
+#		and then use a for loop to print the second list. Make sure each new pizza is stored in the 
+#		approprpiate list. 
+
+pizzas = ["pepperoni", "sausage", "margherita", "veggie", "hawaiian"]
+friend_pizzas = pizzas[:]
+
+pizzas.append('meat lovers')
+friend_pizzas.append('buffalo chicken')
+
+for pizza in pizzas:
+	print(f"I like {pizza} pizza.")
+
+print(f"My favorite pizzas are: ")
+for pizza in pizzas:
+	print(pizza)
+
+print(f"My friend's favorite pizzas are: ")
+for friend_pizza in friend_pizzas:
+	print(friend_pizza)
+
+
+
+# 4-12
+my_foods = ['sushi', 'hopia baboy', 'xiao long bao', 'pho', 'korean bbq']
+lisa_foods = ['sushi', 'pho', 'daikon soup', 'seaweed salad', 'ramen']
+
+for my_food in my_foods:
+	print(my_food)
+for lisa_food in lisa_foods:
+	print(lisa_food)
