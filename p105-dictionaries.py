9b0e36bb0bf65be0683c54474246b0f6f91504e9
mateospencer <matt@mattspencer.net>
2020-04-22 09:44:18 +1200

    added 6-4 exercise

---
+++ p105-dictionaries.py
@@ -0,0 +1,9 @@
+#	Python Crash Course 2nd Edition
+#	Page 105 (Dictionaries)
+
+#	Section 6-4: Glossary 2
+#	Now that you know how to loop through a dictionary, clean up the code from Exercise 6-3
+#	(page 99) by replacing your series of print() calls with a loop that runs through the
+#	dictionary's keys and values. When you're sure that your loop works, add five more Python terms 
+#	to your glossary. When you run your progam again, these new words and meanings should
+#	automatically be included in the output. 
