9a2cdcf3c859abe748a8695552e2f29386b12f20
mateospencer <matt@mattspencer.net>
2020-04-22 10:03:49 +1200

    added 6-4 solution.

--- p105-dictionaries.py
+++ p105-dictionaries.py
@@ -7,3 +7,17 @@
 #	dictionary's keys and values. When you're sure that your loop works, add five more Python terms 
 #	to your glossary. When you run your progam again, these new words and meanings should
 #	automatically be included in the output. 
+
+glossary = {'dictionary': "a collection of key-value pairs.",
+			'string': "a series of characters.",
+			'method': "an action performed ona  piece of data.",
+			'key value pair': "a set of values associated with each other.",
+			'pop()': "a method that removes the last item in a list.",
+			'append()': "a method that adds a new element to the end of a list.",
+			'list': "a collection of items in a particular order.",
+			'constant': "like a variable whose value stays the same through the life of a program.",
+			}
+
+for term in glossary.keys():
+	definition = glossary[term]
+	print("A " + term + " is " + definition)
