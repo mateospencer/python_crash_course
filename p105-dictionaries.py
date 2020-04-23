b1db6e23c07579c87d3757ff7e33536e3c1eb715
mateospencer <matt@mattspencer.net>
2020-04-23 13:48:10 +1200

    added 6-5 exercise

--- p105-dictionaries.py
+++ p105-dictionaries.py
@@ -21,3 +21,13 @@ glossary = {'dictionary': "a collection of key-value pairs.",
 for term in glossary.keys():
 	definition = glossary[term]
 	print("A " + term + " is " + definition)
+
+
+#	Section 6-5: Rivers
+#	Make a dictionary containing three major rivers and the country each river runs through. One
+#	key-value pair might be 'nile': 'egypt'
+#	*	Use a loop to print a sentence about each river, such as The Nile runs through Egypt
+#	*	Use a loop to print the name of each river included in the dictionary. 
+#	*	Use a loop to print the name of each country included in the dictionary. 
+
+rivers = {''}
