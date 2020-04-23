e9856d75d28f0ca4fd7a171aa4160aba09065ceb
mateospencer <matt@mattspencer.net>
2020-04-24 09:19:49 +1200

    added 6-7 exercise

---
+++ p112-nesting.py
@@ -0,0 +1,8 @@
+#	Python Crash Course 2nd Edition
+#	Page 112 (Nesting)
+
+#	Section 6-7: People
+#	Start witht he program you wrote for Exercise 6-1 (page 99). Make two new dictionaries
+#	representing different people, and store all three dictionaries in a list called people. Loop
+#	through your list of people. As you loop through the list, print everything you know about each
+#	person. 
