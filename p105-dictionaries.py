7bacb9e14eb4b821498d5127abc4ae7600421176
mateospencer <matt@mattspencer.net>
2020-04-23 13:50:36 +1200

    added 6-6 exercise

--- p105-dictionaries.py
+++ p105-dictionaries.py
@@ -31,3 +31,14 @@ for term in glossary.keys():
 #	*	Use a loop to print the name of each country included in the dictionary. 
 
 rivers = {''}
+
+
+#	Section 6-6: Polling
+#	Use the code in favorite_languages.py (page 97).
+#	*	Make a list of people who should take the favorite languages poll. Include some names that
+#		are already in the dictionary and some that are not.
+#	*	Loop through the list of people who should take the poll. If they have already taken the
+#		poll, print a message thanking them for responding. If they have not yet taken the poll, 
+#		print a message inviting them to take the poll. 
+
+
