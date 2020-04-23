148636ef6337166af97c814620bc8a4c4d1626d0
mateospencer <matt@mattspencer.net>
2020-04-24 11:08:28 +1200

    added 6-7 solution

--- p112-nesting.py
+++ p112-nesting.py
@@ -2,7 +2,18 @@
 #	Page 112 (Nesting)
 
 #	Section 6-7: People
-#	Start witht he program you wrote for Exercise 6-1 (page 99). Make two new dictionaries
+#	Start with the program you wrote for Exercise 6-1 (page 99). Make two new dictionaries
 #	representing different people, and store all three dictionaries in a list called people. Loop
 #	through your list of people. As you loop through the list, print everything you know about each
 #	person. 
+
+person_0 = {'first_name': 'Jacinda', 'last_name': 'Ardern', 'age': '39', 'city': 'Wellington',}
+person_1 = {'first_name': 'Barack', 'last_name': 'Obama', 'age': '58', 'city': 'Washington',}
+person_2 = {'first_name': 'Justin', 'last_name': 'Trudeau', 'age': '48', 'city': 'Ottawa',}
+
+people = [person_0, person_1, person_2]
+
+for person in people:
+	print("\nFull Name: " + person['first_name'] + " " + person['last_name'])
+	print("Age: " + person['age'])
+	print("City: " + person['city'])
