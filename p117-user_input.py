23792dcef56e0de0124a84be801afa34c002771f
mateospencer <matt@mattspencer.net>
2020-04-26 21:28:32 +1200

    exercise 7-1 added

---
+++ p117-user_input.py
@@ -0,0 +1,6 @@
+#	Python Crash Course 2nd Edition
+#	Page 117 (User Input)
+
+#	Section 7-1: Rental Car
+#	Write a program that asks the user what kind of rental car they would like. Print a message
+#	about that car, such as "Let me see if I can find you a Subaru."
