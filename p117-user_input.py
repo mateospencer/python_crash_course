d48af9df74f27b84efe00dcb7bd84f474a08d06d
mateospencer <matt@mattspencer.net>
2020-04-26 21:55:27 +1200

    solution added for 7-3

--- p117-user_input.py
+++ p117-user_input.py
@@ -27,3 +27,10 @@ else:
 
 #	Section 7-3: Multiples of Ten
 #	Ask the user for a number, and then report whether the number is a multiple of 10 or not. 
+
+number = input("\nEnter a number and I will tell you if its a multiple of 10: ")
+number = int(number)
+if number % 10 == 0:
+	print(f"\nThe number {number} is a mulitiple of 10.")
+else:
+	print(f"\nThe number {number} is not a multiple of 10.")
