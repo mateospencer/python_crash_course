0ab6f3d2404b055c1175b064c7707d75a4e5b26f
mateospencer <matt@mattspencer.net>
2020-04-26 21:31:07 +1200

    solution added to 7-1

--- p117-user_input.py
+++ p117-user_input.py
@@ -4,3 +4,6 @@
 #	Section 7-1: Rental Car
 #	Write a program that asks the user what kind of rental car they would like. Print a message
 #	about that car, such as "Let me see if I can find you a Subaru."
+
+car_choice = input("What type of rental car would you like? ")
+print(f"\nI'll see if we have a {car_choice} available.")
