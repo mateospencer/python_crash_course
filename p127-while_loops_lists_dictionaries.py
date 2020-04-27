7ee45c2dc84ef82553caee33f7b9cd354acbd1aa
mateospencer <matt@mattspencer.net>
2020-04-28 10:42:30 +1200

    exercise added for 7-8

---
+++ p127-while_loops_lists_dictionaries.py
@@ -0,0 +1,9 @@
+#	Python Crash Course 2nd Edition
+#	Page 127 (While Loops with Lists and Dictionaries)
+
+#	Section 7-8: Deli
+#	Make a list called sandwich_orders and fill it with the names of various sandwiches. Then make
+#	an empty list called finished_sandiwches. Loop through the list of sandwich orders and print a 
+#	message for each order, such as I made your tuna sandiwch. As each sandwich is made, move it to
+#	the list list of finished sandwiches. After all the sandwiches have been made, print a message 
+#	listing each sandwich that was made.
