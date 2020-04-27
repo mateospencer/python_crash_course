acda7a6d5ab6d17ae16cee85c6e69b771f2cc8d9
mateospencer <matt@mattspencer.net>
2020-04-27 20:46:18 +1200

    exercise added for 7-5

--- p123-while_loops.py
+++ p123-while_loops.py
@@ -16,3 +16,10 @@ while True:
 		break
 	else:
 		print(f"I'll add [topping] your pizza.")
+
+
+#	Section 7-5: Movie Tickets
+#	A movie theatre charges different ticket prices depending on a person's age. If a person is
+#	under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10; and
+#	if they are over age 12, the ticket is $15. Write a loop in which you ask users their age, and
+#	then tell them the cost of their movie ticket. 
