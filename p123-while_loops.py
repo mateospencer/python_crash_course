fbbee8f2feacc07cfe7b4b2268e182c7610fcfcc
mateospencer <matt@mattspencer.net>
2020-04-27 20:37:16 +1200

    exercise added for 7-4

---
+++ p123-while_loops.py
@@ -0,0 +1,6 @@
+#	Python Crash Course 2nd Edition
+#	Page 123 (While Loops)
+
+#	Section 7-4: Pizza Toppings
+#	Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit'
+#	value. As they enter each topping, print a message saying ou'll add that topping to their pizza.
