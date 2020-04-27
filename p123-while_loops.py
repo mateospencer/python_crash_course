22b133798188386f7d141cd37d42b4e2f23c0a86
mateospencer <matt@mattspencer.net>
2020-04-27 21:30:16 +1200

    fixed typos on 7-6

--- p123-while_loops.py
+++ p123-while_loops.py
@@ -42,8 +42,9 @@ while True:
 
 
 #	Section 7-6: Three Exits
-#	Write different versions of either Exercise 7-4 or Exercise 7-5 taht do each of teh following at
+#	Write different versions of either Exercise 7-4 or Exercise 7-5 that do each of the following at
 #	least once:
 #	*	Use a conditional test in the while statement to stop the loop. 
 #	*	Use an active variable to control how long the loop runs. 
 #	*	Use a break statement to exit the loop when the user enters a 'quit' value.
+
