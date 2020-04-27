67cd262405a514f64f221ff6105f7b9e169a8a37
mateospencer <matt@mattspencer.net>
2020-04-27 21:29:16 +1200

    exercise added for 7-6

--- p123-while_loops.py
+++ p123-while_loops.py
@@ -40,3 +40,10 @@ while True:
 	if age <3:
 		print("$0")
 
+
+#	Section 7-6: Three Exits
+#	Write different versions of either Exercise 7-4 or Exercise 7-5 taht do each of teh following at
+#	least once:
+#	*	Use a conditional test in the while statement to stop the loop. 
+#	*	Use an active variable to control how long the loop runs. 
+#	*	Use a break statement to exit the loop when the user enters a 'quit' value.
