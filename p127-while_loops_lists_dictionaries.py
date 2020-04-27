72fddc1b2e84852ed34cb610214137f2e2e09cb1
mateospencer <matt@mattspencer.net>
2020-04-28 10:55:59 +1200

    exercise added for 7-9

--- p127-while_loops_lists_dictionaries.py
+++ p127-while_loops_lists_dictionaries.py
@@ -21,3 +21,11 @@ while sandwich_orders:
 print("\nThe following sandiwch orders have been made:")
 for finished_sandwich in finished_sandwiches:
 	print(finished_sandwich.title())
+
+
+#	Section 7-9: No Pastrami
+#	Using the list sandwich_orders from Exercise 7-8, make sure the sandiwch 'pastrami' appears in
+#	the list at least three times. Add code near the beginning of your program to print a message
+#	saying the deli has run out of pastrami, and then use a while loop to remove all occurrences of
+#	'pastrami' from sandwich_orders. Make sure no pastrami sandwichesend up in finished_sandwiches.
+
