7680cf6fcaf098f44118b5dc1b0caa6aeaa7f103
mateospencer <matt@mattspencer.net>
2020-04-27 20:42:20 +1200

    solution added for 7-4

--- p123-while_loops.py
+++ p123-while_loops.py
@@ -3,4 +3,16 @@
 
 #	Section 7-4: Pizza Toppings
 #	Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit'
-#	value. As they enter each topping, print a message saying ou'll add that topping to their pizza.
+#	value. As they enter each topping, print a message saying you'll add that topping to their 
+#	pizza.
+
+prompt = "\nEnter your choice of pizza toppings:"
+prompt += "\n(Enter 'quit' when you are finished.)"
+
+while True:
+	topping = input(prompt)
+
+	if topping == 'quit':
+		break
+	else:
+		print(f"I'll add [topping] your pizza.")
