78ae5411f67904395b48a8f5445d7373666eb9b8
mateospencer <matt@mattspencer.net>
2020-04-29 09:47:29 +1200

    exercise added for 8-7

--- p142-function_return_values.py
+++ p142-function_return_values.py
@@ -21,5 +21,14 @@ print(locale2)
 print(locale3)
 
 
-
+#	Section 8-7: Album
+#	Write a function called make_album() that builds a dictionary describing a music album. The 
+#	function should take in an artist name and an album title, and it should return a dictionary
+#	containing these two pieces of information. Use the function to make three dictionaries
+#	representing different albums. Print each return value to show that the dictionaries are storing
+#	the album information correctly. 
+#	Use None to add an optional parameter to make_album() that allows you to store the number of
+#	songs on an album. If the calling line includes a value for the number of songs, add that value
+#	to the album's dictionary. Make at least one new function call that includes the number of songs
+#	on an album. 
 
