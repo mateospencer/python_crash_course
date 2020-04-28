2712c349180d36cf68a80e3287b46c9af6786e6e
mateospencer <matt@mattspencer.net>
2020-04-29 09:44:53 +1200

    solution added for 8-6

--- p142-function_return_values.py
+++ p142-function_return_values.py
@@ -7,3 +7,19 @@
 #		"Santiago, Chile"
 #	Call your function with at least three city-country pairs, and print the values that are 
 #	returned.
+
+def city_country(city, country):
+	"""Returns a city and country neatly formatted"""
+	place_names = f"{city}, {country}"
+	return place_names.title()
+
+locale1 = city_country('Wellington', 'New Zealand')
+locale2 = city_country('Bali', 'Indonesia')
+locale3 = city_country('Mexico City', 'Mexico')
+print(locale1)
+print(locale2)
+print(locale3)
+
+
+
+
