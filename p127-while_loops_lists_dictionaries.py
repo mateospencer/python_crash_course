ffbf50136e8d95b703dd1aa21ce1e0a7bd9bc83b
mateospencer <matt@mattspencer.net>
2020-04-28 12:21:01 +1200

    solution added for 7-10

--- p127-while_loops_lists_dictionaries.py
+++ p127-while_loops_lists_dictionaries.py
@@ -55,3 +55,21 @@ for finished_sandwich in finished_sandwiches:
 #	Write a program that polls users about their dream vacation. Write a prompt similar to if you 
 #	could visit one place in the world, where would you go? Include a block of code that prints the
 #	results of the poll. 
+
+responses = {}
+
+polling_active = True
+
+while polling_active:
+	name = input("\nWhat is your name? ")
+	response = input("If you could visit one place in the world, where would you go? ")
+
+	responses[name] = response
+
+	repeat = input("Would you like to let another person response? (yes/ no) ")
+	if repeat == 'no':
+		polling_active = False
+
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+	print(f"{name} would like to go to {response}.")
