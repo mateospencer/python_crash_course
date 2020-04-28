afa253367723a85d7368a357f4cbf84522f66e9c
mateospencer <matt@mattspencer.net>
2020-04-28 12:45:52 +1200

    exercise added for 8-1

---
+++ p131-functions.py
@@ -0,0 +1,6 @@
+#	Python Crash Course 2nd Edition
+#	Page 131 (Functions)
+
+#	Section 8-1: Message
+#	Write a function called display_message() that pritns one sentence telling everyone what you are
+#	learning about in this chapter. Call the function, and make sure the message displays correctly.
