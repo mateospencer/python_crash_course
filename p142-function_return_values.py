7e4a1d8d4ace179cf4aefd5b1d0f804392f407f3
mateospencer <matt@mattspencer.net>
2020-04-29 10:55:55 +1200

    solution added for 8-7

--- p142-function_return_values.py
+++ p142-function_return_values.py
@@ -32,3 +32,20 @@ print(locale3)
 #	to the album's dictionary. Make at least one new function call that includes the number of songs
 #	on an album. 
 
+def make_album(artist_name, album_title, tracks=None):
+	"""Returns neatly formatted information about a music album"""
+	album = {'artist': artist_name, 'album': album_title}
+	if tracks:
+		album['tracks'] = tracks
+	return album
+
+album1 = make_album('The Roots', 'The Roots Come Alive')
+album2 = make_album('A Tribe Called Quest', 'Midnight Marauders')
+album3 = make_album('Mos Def', 'Black on Both Sides')
+album4 = make_album('Nas', 'Illmatic', '10')
+print(album1)
+print(album2)
+print(album3)
+print(album4)
+
+
