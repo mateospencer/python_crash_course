8c12b50443869f9788a3d06a7b1ec3dcc09ed314
mateospencer <matt@mattspencer.net>
2020-04-28 21:31:16 +1200

    solution added for 8-5

--- p137-Functions_Passing_Arguments.py
+++ p137-Functions_Passing_Arguments.py
@@ -38,3 +38,11 @@ make_shirt(text='Coding in Parseltongue.')
 #	function should print a simple sentence, such as Reykjavik is in Iceland. Give the parameter for
 #	the country a default value. Call your function for three different cities, at least one of
 #	which is not in the default country. 
+
+def describe_city(city, country='New Zealand'):
+	"""Accepts a city and a country and prints a simple sentence with those values."""
+	print(f"\nThe city of {city} is in {country}.")
+
+describe_city('Wellington')
+describe_city('Paris', 'France')
+describe_city(city='Tokyo', country='Japan')
