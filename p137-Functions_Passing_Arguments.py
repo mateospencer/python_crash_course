58b54905faf0c5ceb117458e9cdbdec6c92c3ca5
mateospencer <matt@mattspencer.net>
2020-04-28 21:05:32 +1200

    exercise added for 8-3

---
+++ p137-Functions_Passing_Arguments.py
@@ -0,0 +1,9 @@
+#	Python Crash Course 2nd Edition
+#	Page 137 (Functions and Passing Arguments)
+
+#	Section 8-3: T-Shirt
+#	Write a function called make_shirt() that accepts a size and the text of a message that should
+#	be printed on the shirt. The function should print a sentence summarizing the size of the shirt
+#	and the message printed on it. 
+#	Call the funciton once using positional arguments to make a shirt. Call the function a second
+#	time using keyword arguments. 
