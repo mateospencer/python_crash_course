21a6df6431b8143cd4db173f04179bb5779ed7c8
mateospencer <matt@mattspencer.net>
2020-04-28 12:52:42 +1200

    exercise added for 8-2

--- p131-functions.py
+++ p131-functions.py
@@ -12,3 +12,10 @@ def display_message():
 	" to a function, and about arguments and parameters.")
 
 display_message()
+
+
+#	Section 8-2: Favorite Book
+#	Write a function called favorite_book() that accepts one parameter, title. The function should
+#	print a message, such as One of my favorite books is Alice in Wonderland. Call the function, 
+#	making sure to include a book title as an argument in the function call. 
+
