833d58c4e321da5c8c9fa2d5718ba1655002c2f7
mateospencer <matt@mattspencer.net>
2020-04-28 12:51:22 +1200

    solution added for 8-1

--- p131-functions.py
+++ p131-functions.py
@@ -4,3 +4,11 @@
 #	Section 8-1: Message
 #	Write a function called display_message() that pritns one sentence telling everyone what you are
 #	learning about in this chapter. Call the function, and make sure the message displays correctly.
+
+def display_message():
+	"""Displays what I am learning in this chapter."""
+	print("In Chapter 8, we learn about functions, or named blocks of code that are designed"
+	" to do one specific job. Specifically, we learn how to define a function, how to pass info"
+	" to a function, and about arguments and parameters.")
+
+display_message()
