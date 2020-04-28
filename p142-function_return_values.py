bba3ba9139cfb4defeb91f5eeb5868de568bd9cf
mateospencer <matt@mattspencer.net>
2020-04-29 09:37:05 +1200

    exercise added for 8-6

---
+++ p142-function_return_values.py
@@ -0,0 +1,9 @@
+#	Python Crash Course 2nd Edition
+#	Page 142 (Functions and Returning Values)
+
+#	Section 8-6: City Names
+#	Write a function called city_country() that takes in the name of a city and its country. The
+#	function should return a string formatted like this:
+#		"Santiago, Chile"
+#	Call your function with at least three city-country pairs, and print the values that are 
+#	returned.
