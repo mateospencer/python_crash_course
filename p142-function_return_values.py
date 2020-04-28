d99da148f47b75a9a23c1c57f66322a3308f16d1
mateospencer <matt@mattspencer.net>
2020-04-29 10:57:47 +1200

    exercise added for 8-8

--- p142-function_return_values.py
+++ p142-function_return_values.py
@@ -49,3 +49,10 @@ print(album3)
 print(album4)
 
 
+#	Section 8-8: User Albums
+#	Start with your program from Exercise 8-7. Write a while loop that allows uers to enter an 
+#	album's artist and title. Once you have that information, call make_album() with the user's
+#	input and print the dictionary that's created. Be sure to include a quit value in the while
+#	loop. 
+
+
