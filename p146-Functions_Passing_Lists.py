1a6bfe9120a8cc1186a15422101582dc4c2c8431
mateospencer <matt@mattspencer.net>
2020-04-29 21:11:45 +1200

    exercise added for 8-9

---
+++ p146-Functions_Passing_Lists.py
@@ -0,0 +1,6 @@
+#	Python Crash Course 2nd Edition
+#	Page 146 (Functions Passing Lists)
+
+#	Section 8-9: Messages
+#	Make a list containing a series of short text messages. Pass the list to a function called
+#	show_messages(), which prints each text message. 
