f9e93b12bf55f43c987385f8d1cf34046d1a04df
mateospencer <matt@mattspencer.net>
2020-04-30 10:27:27 +1200

    exercise added for 8-12

---
+++ p150-Functions_Arbitrary_Arguments.py
@@ -0,0 +1,8 @@
+#	Python Crash Course 2nd Edition
+#	Page 150 (Arbitrary Number of Arguments)
+
+#	Section 8-12: Sandwiches
+#	Write a function that accepts a list of items a person wants on a sandwich. The function should
+#	have one parameter that collects as many items as the function call provides, and it should
+#	print a summary of the sandwich that's being ordered. Call the function three times, using a 
+#	different number of arguments each time. 
