8c42b37631fdcfefecd4dbc0f598f35f60812918
mateospencer <matt@mattspencer.net>
2020-04-29 21:41:21 +1200

    exercise added for 8-10

--- p146-Functions_Passing_Lists.py
+++ p146-Functions_Passing_Lists.py
@@ -12,3 +12,10 @@ def show_messages(messages):
 
 messages = ["On my way!", "No worries.", "I love you.", "Take care."]
 show_messages(messages)
+
+
+#	Section 8-10: Sending Messages
+#	Start with a copy of your program from Exercise 8-9. Write a function called send_messages()
+#	that prints each text message and moves each message to a new list called sent_messages as it's 
+#	printed. After calling the function, print both of your lists to make sure the messages were 
+#	moved correctly. 
