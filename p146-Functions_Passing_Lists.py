8d7fecd1d7f87bbcce5531b36c3a94dc45920726
mateospencer <matt@mattspencer.net>
2020-04-29 21:58:18 +1200

    solution added for 8-10

--- p146-Functions_Passing_Lists.py
+++ p146-Functions_Passing_Lists.py
@@ -19,3 +19,27 @@ show_messages(messages)
 #	that prints each text message and moves each message to a new list called sent_messages as it's 
 #	printed. After calling the function, print both of your lists to make sure the messages were 
 #	moved correctly. 
+
+def show_messages(unsent_messages):
+	"""Prints messages from a list of messages"""
+	for message in unsent_messages:
+		print(message)
+
+def send_messages(unsent_messages, sent_messages):
+	"""Simulates sending a message and moving it from an unsent list to sent list."""
+	while unsent_messages:
+		current_message = unsent_messages.pop()
+		print(current_message)
+		sent_messages.append(current_message)
+
+unsent_messages = ["On my way!", "No worries.", "I love you.", "Take care."]
+sent_messages = []
+
+show_messages(unsent_messages)
+send_messages(unsent_messages, sent_messages)
+
+print("The following messages failed to send:")
+print(unsent_messages)
+
+print("The following messages were successfuly sent:")
+print(sent_messages)
