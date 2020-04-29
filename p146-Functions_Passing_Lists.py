49f937d174b0cc3b8190635a97a09afc7fdee57a
mateospencer <matt@mattspencer.net>
2020-04-29 21:39:51 +1200

    solution added for 8-9

--- p146-Functions_Passing_Lists.py
+++ p146-Functions_Passing_Lists.py
@@ -4,3 +4,11 @@
 #	Section 8-9: Messages
 #	Make a list containing a series of short text messages. Pass the list to a function called
 #	show_messages(), which prints each text message. 
+
+def show_messages(messages):
+	"""Prints messages from a list"""
+	for message in messages:
+		print(message)
+
+messages = ["On my way!", "No worries.", "I love you.", "Take care."]
+show_messages(messages)
