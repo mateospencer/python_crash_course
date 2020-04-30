3b29a1722fa41018a110cbebe6ce406d9b7ba57b
mateospencer <matt@mattspencer.net>
2020-04-30 21:27:07 +1200

    solution added for 8-13

--- p150-Functions_Arbitrary_Arguments.py
+++ p150-Functions_Arbitrary_Arguments.py
@@ -19,3 +19,13 @@ make_sandwich('cheese')
 #	Start with a copy of user_profile.py from page 149. Build a profile of yourself by calling
 #	build_profile(), using your first and last names and three other key-value pairs that describe
 #	you. 
+
+def build_profile(first, last, **user_info):
+	"""Build a dictionary containing everything we know about a user."""
+	user_info['first_name'] = first
+	user_info['last_name'] = last
+	return user_info
+
+user_profile = build_profile('mateo', 'spencer', city='wellington', country='new zealand', 
+	title='lead data scientist')
+print(user_profile)
