da6662aff67b7cd5bc6ddbd473f6513911814ad6
mateospencer <matt@mattspencer.net>
2020-05-01 09:07:20 +1200

    exercise added for 8-15

---
+++ p155-modules.py
@@ -0,0 +1,7 @@
+#	Python Crash Course 2nd Edition
+#	Page 155 (Functions in Modules)
+
+#	Section 8-15: Printing Models
+#	Put the functions for the example printing_models.py in a separate file called
+#	printing_functions.py. Write an import statement at the top of printing_models.py, and modify
+#	the file to use the imported functions. 
