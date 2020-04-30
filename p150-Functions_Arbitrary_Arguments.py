37ac7327674e981d14e221d62636ba72db5e108b
mateospencer <matt@mattspencer.net>
2020-04-30 21:29:44 +1200

    exercise added for 8-14

--- p150-Functions_Arbitrary_Arguments.py
+++ p150-Functions_Arbitrary_Arguments.py
@@ -29,3 +29,12 @@ def build_profile(first, last, **user_info):
 user_profile = build_profile('mateo', 'spencer', city='wellington', country='new zealand', 
 	title='lead data scientist')
 print(user_profile)
+
+
+#	Section 8-14: Cars
+#	Write a function that stores information about a car in a dictonary. The function should always
+#	receive a manufacturer and a model name. It should then accept an arbitrary number of keyword
+#	arguments. Call the function with the required information and two other name-value pairs, such
+#	as a color or an option feature. Your function should work for a call like this one:
+#		car = make_car('subaru', 'outback', color='blue', tow_package=True)
+#	Print the dictionary that's returned to make sure all the information was stored correctly. 
