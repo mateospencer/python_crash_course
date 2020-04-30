63549306752c3c73b0a68b259ff16d17320f5212
mateospencer <matt@mattspencer.net>
2020-04-30 21:23:51 +1200

    solution added for 8-12

--- p150-Functions_Arbitrary_Arguments.py
+++ p150-Functions_Arbitrary_Arguments.py
@@ -6,3 +6,12 @@
 #	have one parameter that collects as many items as the function call provides, and it should
 #	print a summary of the sandwich that's being ordered. Call the function three times, using a 
 #	different number of arguments each time. 
+
+def make_sandwich(*toppings):
+	print(toppings)
+
+make_sandwich('bacon', 'lettuce', 'tomato',)
+make_sandwich('peanut butter', 'jelly',)
+make_sandwich('cheese')
+
+
