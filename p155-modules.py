e08357fb24ff51921c2e47f93302e31ff8bc729a
mateospencer <matt@mattspencer.net>
2020-05-01 13:21:56 +1200

    exercise added for 8-16

--- p155-modules.py
+++ p155-modules.py
@@ -16,3 +16,14 @@ completed_models = []
 
 pf.print_models(unprinted_designs, completed_models)
 pf.show_completed_models(completed_models)
+
+
+#	Section 8-16: Imports
+#	Using a program you wrote that has one function in it, store that function in a separate file.
+#	Import the function into your main program file, and call the function using each of these
+#	approaches:
+#		*	import module_name
+#		*	from module_name import function_name
+#		*	from module_name import function_name as fn
+#		*	import module_name as mn
+#		*	from module_name import *
