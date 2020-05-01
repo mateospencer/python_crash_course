e68e9b101b07cad2065a09266a975f69a07b77ac
mateospencer <matt@mattspencer.net>
2020-05-01 14:20:12 +1200

    exercise added for 8-17

--- p155-modules.py
+++ p155-modules.py
@@ -50,5 +50,7 @@ print(etron)
 
 
 #	Section 8-17: Styling Functions
+#	Choose any three programs you wrote for this chapter, and make sure they follow the styling 
+#	guidelines described in this section.
 
 
