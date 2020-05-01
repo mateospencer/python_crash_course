f0e97d1db4ea75761941e7b4827fa7644071cd6a
mateospencer <matt@mattspencer.net>
2020-05-01 13:13:57 +1200

    updated printing_models.py to use imports

--- p155-modules.py
+++ p155-modules.py
@@ -5,3 +5,11 @@
 #	Put the functions for the example printing_models.py in a separate file called
 #	printing_functions.py. Write an import statement at the top of printing_models.py, and modify
 #	the file to use the imported functions. 
+
+import printing_functions as pf
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
