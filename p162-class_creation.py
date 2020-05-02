4c919360c31e41eb593e2114af0edfefa94925c0
mateospencer <matt@mattspencer.net>
2020-05-02 21:17:49 +1200

    exercise added for 9-3

--- p162-class_creation.py
+++ p162-class_creation.py
@@ -45,3 +45,11 @@ restaurant4 = Restaurant('Yama', 'Japanese')
 restaurant2.describe_restaurant()
 restaurant3.describe_restaurant()
 restaurant4.describe_restaurant()
+
+
+#	Section 9-3: Users
+#	Make a class called User. Create two attributes called first_name and last_name, and then create
+#	several other attributes that are typically stored in a user profile. Make a method called
+#	describe_user() that prints a summary of the user's information. Make another method called 
+#	greet_user() that prints a personalized greeting to the user. 
+#	Create several instances representing different users, and call both methods for each user. 
