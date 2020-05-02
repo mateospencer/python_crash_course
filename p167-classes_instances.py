bca457d2e23ff5996cf8a0c76455fe0dc5a0d3cc
mateospencer <matt@mattspencer.net>
2020-05-02 22:43:31 +1200

    exercise added for 9-4

---
+++ p167-classes_instances.py
@@ -0,0 +1,12 @@
+#	Python Crash Course 2nd Edition
+#	Page 167 (Working with Classes and Instances)
+
+#	Section 9-4: Number Served
+#	Start with your program from Exercise 9-1 (page 162). Add an attribute called number_served with
+#	a default value of 0. Create an instance called restaurant from this class. Print the number of
+#	customers the restaurant has served, and then change this value and print it again. 
+#	Add a method called set_number_served() that lets you set the number of customers that have been
+#	served. Call this method with a new number and print the value again. 
+#	Add a method called increment_number_served() that lets you increment the number of customers
+#	who've been served. Call this method with any number you like that could represent how many
+#	customers were served in, say, a day of business. 
