54a6973a8d354ce7591fa228a8f40ce2c40b2fe3
mateospencer <matt@mattspencer.net>
2020-05-02 22:45:22 +1200

    saving work in progress for 9-4

--- p167-classes_instances.py
+++ p167-classes_instances.py
@@ -10,3 +10,21 @@
 #	Add a method called increment_number_served() that lets you increment the number of customers
 #	who've been served. Call this method with any number you like that could represent how many
 #	customers were served in, say, a day of business. 
+
+class Restaurant:
+	"""A simple class representing restaurant details"""
+	def __init__(self, restaurant_name, cuisine_type):
+		"""Initialize restaurant name and cuisine type attributes"""
+		self.restaurant_name = restaurant_name.title()
+		self.cuisine_type = cuisine_type
+		self.number_served = 0
+
+	def describe_restaurant(self):
+		"""Prints info about the restaurant."""
+		details = f"{self.restaurant_name} is a {self.cuisine_type} restaurant."
+		print(f"\n{details}")
+
+	def open_restaurant(self):
+		"""Prints a message statin that the restaurant is open."""
+		open = f"{self.restaurant_name} is now open."
+		print(f"\n{open}")
