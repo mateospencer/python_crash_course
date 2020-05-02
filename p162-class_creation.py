2331bbf0cc1199a0aa0529db22bbf662bc7cbd17
mateospencer <matt@mattspencer.net>
2020-05-02 13:18:54 +1200

    exercise added for 9-1

---
+++ p162-class_creation.py
@@ -0,0 +1,10 @@
+#	Python Crash Course 2nd Edition
+#	Page 162 (Creating and Using Classes)
+
+#	Section 9-1: Restaurant
+#	Make a class called Restaurant. The __init__() method for Restaurant should store two
+#	attributes: a restaurant_name and a cuisine_type. Make a method called describe_restaurant()
+#	that prints these two pieces of information, and a method called open_restaurant() that prints
+#	a message indicating that the restaurant is open. 
+#	Make an instance called restaurant from your class. Print the two attributes individually, and
+#	then call both methods. 
