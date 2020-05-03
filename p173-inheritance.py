057a3d41386d6c181f74b629dbbeb5c8ffc9e900
mateospencer <matt@mattspencer.net>
2020-05-03 21:31:37 +1200

    exercise added for 9-6

---
+++ p173-inheritance.py
@@ -0,0 +1,9 @@
+#	Python Crash Course 2nd Edition
+#	Page 173 (Inheritance)
+
+#	Section 9-6: Ice Cream Stand
+#	An ice cream stand is a specific kind of restaurant. Write a clas called IceCreamStand that
+#	inherits from the Restaurant class you wrote in Exercise 9-1 (page 162) or Exercise 9-4
+#	(page 167). Either version of the class will work; just pick the one you like better. Add an 
+#	attribute called flavors that stores a list of ice cream flavors. Write a method that displays
+#	these flavors. Crate an instance of IceCreamStand, and call this method. 
