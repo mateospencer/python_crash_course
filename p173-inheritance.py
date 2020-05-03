90221bf2393a16612dbf8d155272b6117447b13d
mateospencer <matt@mattspencer.net>
2020-05-03 21:57:01 +1200

    exercise added for 9-7

--- p173-inheritance.py
+++ p173-inheritance.py
@@ -53,3 +53,11 @@ icecreamstand1.flavors = ['horchata', 'coffee', 'caramel', 'dulce de leche', 'th
 
 icecreamstand1.describe_restaurant()
 icecreamstand1.display_flavors()
+
+
+#	Section 9-7: Admin
+#	An administrator is a special kind of user. Write a class called Admin that inherits from the
+#	User class you wrote in Exercise 9-3 (page 162) or Exercise 9-5 (page 167). Add an attribute,
+#	privileges, that stores a list of strings like "can add post", "can delete post", 
+#	"can ban user", and so on. Write a method called show_privileges() that lists the
+#	administrator's set of privileges. Create an instance of Admin, and call your method. 
