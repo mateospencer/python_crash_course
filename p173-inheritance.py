8a20697e23ee27fa5f526b3d53468deb244a18b2
mateospencer <matt@mattspencer.net>
2020-05-03 22:44:16 +1200

    solution added for 9-7

--- p173-inheritance.py
+++ p173-inheritance.py
@@ -61,3 +61,49 @@ icecreamstand1.display_flavors()
 #	privileges, that stores a list of strings like "can add post", "can delete post", 
 #	"can ban user", and so on. Write a method called show_privileges() that lists the
 #	administrator's set of privileges. Create an instance of Admin, and call your method. 
+
+class User:
+	"""A class for simulated user profile details"""
+	def __init__(self, first_name, last_name, email, title):
+		"""Initializing attributes"""
+		self.first_name = first_name.title()
+		self.last_name = last_name.title()
+		self.email = email
+		self.title = title.title()
+		self.login_attempts = 0
+
+	def describe_user(self):
+		print(f"User's Name: {self.first_name} {self.last_name}")
+		print(f"Email: {self.email}")
+		print(f"Title: {self.title}")
+
+	def greet_user(self):
+		"""Greets user by username"""
+		print(f"\nKia ora, {self.first_name}!")
+
+	def increment_login_attempts(self):
+		"""Enables incrementing the number of login attempts by the user via a method"""
+		self.login_attempts += 1
+
+	def reset_login_attempts(self):
+		"""Resets login attempts back to zero"""
+		self.login_attempts = 0
+
+class Admin(User):
+	"""A class for admin profile attributes"""
+	def __init__(self, first_name, last_name, email, title):
+		"""Initializing attributes of parent class and those for Admin"""
+		super().__init__(first_name, last_name, email, title)
+		self.privileges = []
+
+	def show_privileges(self):
+		"""Lists the administrator's set of privileges."""
+		print("\nThe Administrator has the following privileges:")
+		for privilege in self.privileges:
+			print(f"{privilege}")
+
+admin_user = Admin('Admin', 'Istrator', 'admin@website.co.nz', 'Admin')
+admin_user.privileges = ['create user', 'delete user', 'reset password', 'ban user']
+
+admin_user.describe_user()
+admin_user.show_privileges()
