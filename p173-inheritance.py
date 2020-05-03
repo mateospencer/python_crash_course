1e75ea39577481ce2c9f44cf09e84a3b3d6395e8
mateospencer <matt@mattspencer.net>
2020-05-03 22:46:43 +1200

    exercise added for 9-8

--- p173-inheritance.py
+++ p173-inheritance.py
@@ -107,3 +107,10 @@ admin_user.privileges = ['create user', 'delete user', 'reset password', 'ban us
 
 admin_user.describe_user()
 admin_user.show_privileges()
+
+
+#	Section 9-8: Privileges
+#	Write a separate Privileges class. The class should have one attribute, privileges, that stores
+#	a list of strings as described in Exercise 9-7. Move teh show_privileges() method to this class.
+#	Make a Privileges instance as an attribute in the Admin class. Create a new instance of Admin 
+#	and use your method to show its privileges.
