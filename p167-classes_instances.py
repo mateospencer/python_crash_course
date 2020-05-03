350e65c853395d1c1ab417b154b60f0533a88630
mateospencer <matt@mattspencer.net>
2020-05-03 12:50:27 +1200

    exercise added for 9-5

--- p167-classes_instances.py
+++ p167-classes_instances.py
@@ -43,6 +43,15 @@ print(restaurant.number_served)
 restaurant.number_served = 314
 print(restaurant.number_served)
 
+
+#	Section 9-5: Login Attempts
+#	Add an attribute called login_attempts to your User calss from Exercise 9-3 (page 162). Write a 
+#	method called increment_login_attempts() that increments the value of login_attempts by 1. 
+#	Write another method called reset_login_attempts() that resets the value of login_attempts to 0.
+#	Make an instance of the User class  and call increment_login_attempts() several times. Print 
+#	the value of login_attempts to make sure it was incremented properly, and then call
+#	reset_login_attempts(). Print login_attempts again to make sure it was reset to 0. 
+
 restaurant.set_number_served(592)
 print(restaurant.number_served)
 
