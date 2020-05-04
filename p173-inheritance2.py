993a2e39320c7ed0acb84d62f17c126514dd2ece
mateospencer <matt@mattspencer.net>
2020-05-04 12:51:23 +1200

    exercise added for 9-9

--- p173-inheritance2.py
+++ p173-inheritance2.py
@@ -64,3 +64,11 @@ admin_privileges = ['backup volume', 'create volume', 'delete volume',]
 
 admin.privileges.privileges = admin_privileges
 admin.privileges.show_privileges()
+
+
+#	Section 9-9: Battery Upgrade
+#	Use the final version of electric_car.py from this section. Add a method to the Battery class
+#	called upgrade_battery(). This method should check the battery size and set the capacity to 100
+#	if it isn't already. Make an electric car with a default battery size, call get_range() once,
+#	and then call get_range() a second time after upgrading the battery. You should see an increase
+#	in the car's range. 
