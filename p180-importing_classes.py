5e7f42743c0bc413b49368abf0a85c2eb5d2e101
mateospencer <matt@mattspencer.net>
2020-05-05 09:34:53 +1200

    exercise added for 9-12

--- p180-importing_classes.py
+++ p180-importing_classes.py
@@ -24,3 +24,9 @@ admin.privileges.privileges = ['backup volume', 'create volume', 'delete volume'
 
 admin.describe_user()
 admin.privileges.show_privileges()
+
+
+#	Section 9-12: Multiple Modules
+#	Store the User class in one module, and store the Privileges and Admin classes in a separate
+#	module. In a separate file, create an Admin instance and call show_privileges() to show that
+#	everything is still working correctly.
