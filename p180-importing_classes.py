8b43c1ee0e203f1977cceab931c4374fac93a5c8
mateospencer <matt@mattspencer.net>
2020-05-04 21:31:50 +1200

    solution added for 9-10

--- p180-importing_classes.py
+++ p180-importing_classes.py
@@ -4,4 +4,9 @@
 #	Section 9-10: Imported Restaurant
 #	Using your latest Restaurant class, store it in a module. Make a separate file that imports
 #	Restaurant. Make a Restaurant instance, and call one of Restaurant's methods to show that the
-#	import statement is working properly. 
+#	import statement is working properly.
+
+from restaurant import *
+
+restaurant = Restaurant('Mercury', 'Tapas')
+restaurant.describe_restaurant()
