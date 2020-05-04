c0147abe8046a0f27cfec2821a341b94cc7efd33
mateospencer <matt@mattspencer.net>
2020-05-05 09:23:36 +1200

    exercise added for 9-11

--- p180-importing_classes.py
+++ p180-importing_classes.py
@@ -10,3 +10,9 @@ from restaurant import *
 
 restaurant = Restaurant('Mercury', 'Tapas')
 restaurant.describe_restaurant()
+
+
+#	Section 9-11: Imported Admin
+#	Start with your wok from Exercise 9-8 (page 173). Store the classes User, Privileges, and Admin
+#	in one module. Create a separate file, make an Admin instance, and call show_privileges() to 
+#	show that everything is working correctly.
