2bae3a7c07a840d083a1ee8d8a408686600b8b4b
mateospencer <matt@mattspencer.net>
2020-05-04 20:27:05 +1200

    exercise added for 9-10

---
+++ p180-importing_classes.py
@@ -0,0 +1,7 @@
+#	Python Crash Course 2nd Edition
+#	Page 180 (Importing Classes)
+
+#	Section 9-10: Imported Restaurant
+#	Using your latest Restaurant class, store it in a module. Make a separate file that imports
+#	Restaurant. Make a Restaurant instance, and call one of Restaurant's methods to show that the
+#	import statement is working properly. 
