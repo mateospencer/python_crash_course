8faeff174f8932b0cfdaa351dc159da2e6db173e
mateospencer <matt@mattspencer.net>
2020-05-05 09:32:43 +1200

    solution added for 9-11

--- p180-importing_classes.py
+++ p180-importing_classes.py
@@ -16,3 +16,11 @@ restaurant.describe_restaurant()
 #	Start with your wok from Exercise 9-8 (page 173). Store the classes User, Privileges, and Admin
 #	in one module. Create a separate file, make an Admin instance, and call show_privileges() to 
 #	show that everything is working correctly.
+
+from user import *
+
+admin = Admin('Admin', 'Istrator', 'admin@website.co.nz', 'Administrator')
+admin.privileges.privileges = ['backup volume', 'create volume', 'delete volume']
+
+admin.describe_user()
+admin.privileges.show_privileges()
