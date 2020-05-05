59eb5aae998d88a7d7ee1278cab8715b0aa970db
mateospencer <matt@mattspencer.net>
2020-05-05 20:03:38 +1200

    exercise added for 9-13

---
+++ p181-python_standard_library.py
@@ -0,0 +1,8 @@
+#	Python Crash Course 2nd Edition
+#	Page 181 (Python Standard Library)
+
+#	Section 9-13: Dice
+#	Make a class Die with one attribute called sides, which has a default value of 6. Write a method
+#	called roll_die() that prints a random number between 1 and the number of sides the die has. 
+#	Make a 6-sided die and roll it 10 times. 
+#	Make a 10-sided die and a 20-sided die. Roll each die 10 times. 
