18e26189f76510e0fe142b36378c6109865f1177
mateospencer <matt@mattspencer.net>
2020-05-05 12:45:18 +1200

    created a module with just user for exercise 9-12

---
+++ user2.py
@@ -0,0 +1,26 @@
+class User:
+	"""A class for simulated user profile details"""
+	def __init__(self, first_name, last_name, email, title):
+		"""Initializing attributes"""
+		self.first_name = first_name.title()
+		self.last_name = last_name.title()
+		self.email = email
+		self.title = title.title()
+		self.login_attempts = 0
+
+	def describe_user(self):
+		print(f"\nUser's Name: {self.first_name} {self.last_name}")
+		print(f"Email: {self.email}")
+		print(f"Title: {self.title}")
+
+	def greet_user(self):
+		"""Greets user by username"""
+		print(f"\nKia ora, {self.first_name}!")
+
+	def increment_login_attempts(self):
+		"""Enables incrementing the number of login attempts by the user via a method"""
+		self.login_attempts += 1
+
+	def reset_login_attempts(self):
+		"""Resets login attempts back to zero"""
+		self.login_attempts = 0
