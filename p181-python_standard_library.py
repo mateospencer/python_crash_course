214381da07542f1856652101b27b570f2f51993b
mateospencer <matt@mattspencer.net>
2020-05-06 20:10:26 +1200

    solution added for 9-13

--- p181-python_standard_library.py
+++ p181-python_standard_library.py
@@ -20,12 +20,43 @@ class Die():
         """Return a number between 1 and the number of sides."""
         return randint(1, self.sides)
 
-# Make a 10-sided die, and show the results of 10 rolls.
+#	Different sided Dice
+d6 = Die()
 d10 = Die(sides=10)
+d20 = Die(sides=20)
 
-results = []
+#	Rolling different sided dice once
+d6roll = d6.roll_die()
+d10roll = d10.roll_die()
+d20roll = d20.roll_die()
+
+print("Rolling a six sided die once: " + str(d6roll))
+print("Rolling a ten sided die once: " + str(d10roll))
+print("Rolling a twenty sided die once: " + str(d20roll))
+
+#	Rolling a d6 ten times
+results6 = []
 for roll_num in range(10):
-    result = d10.roll_die()
-    results.append(result)
-print("\n10 rolls of a 10-sided die:")
-print(results)
+	result = d6.roll_die()
+	results6.append(result)
+print("\nRolling a six sided die ten times:")
+print(results6)
+
+#	Rolling a d10 ten times
+results10 = []
+for roll_num in range(10):
+	result = d10.roll_die()
+	results10.append(result)
+print("\nRolling a ten sided die ten tiems:")
+print(results10)
+
+#	Rolling a d20 ten times
+results20 = []
+for roll_num in range(10):
+	result = d20.roll_die()
+	results20.append(result)
+print("\nRolling a twenty sided die ten times:")
+print(results20)
+
+
+
