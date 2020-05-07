4df24ad1652759d030e68dee994510b30aee93a7
mateospencer <matt@mattspencer.net>
2020-05-07 21:56:00 +1200

    exercise added for 9-15

--- p181-python_standard_library.py
+++ p181-python_standard_library.py
@@ -86,3 +86,9 @@ while len(winning_alphanumerics) < 4:
 		winning_alphanumerics.append(selected_alphanumeric)
 		print(f"A {selected_alphanumeric} is drawn.")
 print("The four winning selections are:" ,winning_alphanumerics)
+
+
+#	Section 9-15: Lottery Analysis
+#	You can use a loop to see how hard it might be to win the kind of lottery you just modeled. Make
+#	a list or tuple called my_ticket. Write a loop that keeps pulling numbers until your ticket 
+#	wins. Print a message reporting how many tiems the loop had to run to give you a winning ticket.
