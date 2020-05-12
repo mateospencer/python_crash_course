f6d6512531bbf5236cd9c549df5b1407fb0d393c
mateospencer <matt@mattspencer.net>
2020-05-13 10:58:34 +1200

    added note to 9-15

--- p181-python_standard_library.py
+++ p181-python_standard_library.py
@@ -93,16 +93,11 @@ print("The four winning selections are:" ,winning_alphanumerics)
 #	a list or tuple called my_ticket. Write a loop that keeps pulling numbers until your ticket 
 #	wins. Print a message reporting how many tiems the loop had to run to give you a winning ticket.
 
+#	Need to admit that I went about this in a very inefficient way and had to finally look up
+#	the solution in the book and follow its format. My initial approach worked very differently and 
+#	needlessly complicated and messy.
+
 from random import choice
-
-#	need to break problem down into parts. 
-
-#	Need to create a class for getting the winning numbers. Could reuse existing code but best to
-#	approach this with a class to do the work
-
-#	Need another class to generating a random entry ticket. 
-
-#	Need a class to check the entry ticket against the winning numbers. 
 
 def generate_winner(alphanumerics):
 	"""Generates the winning numbers for the lottery"""
