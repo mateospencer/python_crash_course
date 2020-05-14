c55969a424ebb36dbea0f1213fb6de48e3507c88
mateospencer <matt@mattspencer.net>
2020-05-15 09:59:20 +1200

    changed alphanumerics (a relic to the original assignment) to numbers

--- powerball.py
+++ powerball.py
@@ -10,33 +10,38 @@
 
 from random import choice
 
-winning_alphanumerics = []
+#	These had to be moved outside of the class definitions because at least one of them was used by
+#	more than one class. They didn't all need to be here but it keeps it consistent.
+
+#	Empty List to store the winning 
+winning_numbers = []
 winning_powerball = []
-picked_alphanumerics = []
+picked_numbers = []
 picked_powerball = []
 
-def generate_winner(alphanumerics):
-	while len(winning_alphanumerics) < 5:
+def generate_winner(numbers):
+	"""Generates the winning numbers for the lottery"""
+	while len(winning_numbers) < 5:
 		#	Randomly select out of our list of possible letters and numbers and assign it a variable
-		drawn_digit = choice(alphanumerics)
+		drawn_digit = choice(numbers)
 		#	Next, will want to make sure that we don't repeat numbers
-		if drawn_digit not in winning_alphanumerics:
+		if drawn_digit not in winning_numbers:
 			#	Add the digit to the list (if not a repeat)
-			winning_alphanumerics.append(drawn_digit)
+			winning_numbers.append(drawn_digit)
 	#	Return the full list of the winning combination
-	return winning_alphanumerics
+	return winning_numbers
 
 def generate_winning_powerball(powerballs):
 	while len(winning_powerball) < 1:
 		select_winning_powerball = choice(powerballs)
-		if select_winning_powerball not in winning_alphanumerics:
+		if select_winning_powerball not in winning_numbers:
 			winning_powerball.append(select_winning_powerball)
 
 	return winning_powerball
 
-def check_ticket(picked_alphanumerics, winning_alphanumerics):
-	for pick in picked_alphanumerics:
-		if pick not in winning_alphanumerics:
+def check_ticket(picked_numbers, winning_numbers):
+	for pick in picked_numbers:
+		if pick not in winning_numbers:
 			return False
 	return True
 
@@ -46,25 +51,25 @@ def check_powerball(picked_powerball, winning_powerball):
 			return False
 	return True
 
-def generate_ticket(alphanumerics):
-	while len(picked_alphanumerics) < 5:
-		drawn_digit = choice(alphanumerics)
-		if drawn_digit not in picked_alphanumerics:
-			picked_alphanumerics.append(drawn_digit)
+def generate_ticket(numbers):
+	while len(picked_numbers) < 5:
+		drawn_digit = choice(numbers)
+		if drawn_digit not in picked_numbers:
+			picked_numbers.append(drawn_digit)
 			
-	return picked_alphanumerics
+	return picked_numbers
 
-def generate_powerball(powerballs, picked_alphanumerics):
+def generate_powerball(powerballs, picked_numbers):
 	while len(picked_powerball) < 1:
 		select_powerball = choice(powerballs)
-		if select_powerball not in picked_alphanumerics:
+		if select_powerball not in picked_numbers:
 			picked_powerball.append(select_powerball)
 
 	return picked_powerball
 
-alphanumerics = range(0, 70)
+numbers = range(0, 70)
 powerballs = range(0, 27)
-winning_alphanumerics = generate_winner(alphanumerics)
+winning_numbers = generate_winner(numbers)
 winning_powerball = generate_winning_powerball(powerballs)
 
 tickets = 0
@@ -74,9 +79,9 @@ hit_powerball = False
 max_tries = 100_000_000
 
 while not won:
-	new_ticket = generate_ticket(alphanumerics)
-	new_powerball = generate_powerball(powerballs, picked_alphanumerics)
-	won = check_ticket(new_ticket, winning_alphanumerics)
+	new_ticket = generate_ticket(numbers)
+	new_powerball = generate_powerball(powerballs, picked_numbers)
+	won = check_ticket(new_ticket, winning_numbers)
 	hit_powerball = check_powerball(picked_powerball, winning_powerball)
 	tickets += 1
 	if tickets >= max_tries:
@@ -86,12 +91,12 @@ if won:
 	print("We have a winner!")
 	print(f"Purchased ticket: {new_ticket}")
 	print(f"Purchased powerball: {new_powerball}")
-	print(f"Winning ticket: {winning_alphanumerics}")
+	print(f"Winning ticket: {winning_numbers}")
 	print(f"Winning Powerball: {winning_powerball}")
 	print(f"It took {tickets} tries to win!")
 else:
     print(f"Tried {tickets} times, without pulling a winner. :(")
     print(f"Purchased ticket: {new_ticket}")
     print(f"Purchased powerball: {new_powerball}")
-    print(f"Winning ticket: {winning_alphanumerics}")
+    print(f"Winning ticket: {winning_numbers}")
     print(f"Winning powerball: {winning_powerball}")
