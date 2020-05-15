ddb9edddf2c38c562de11814c98c100b74cf87ee
mateospencer <matt@mattspencer.net>
2020-05-15 20:15:57 +1200

    documentation cleanup

--- powerball.py
+++ powerball.py
@@ -24,7 +24,7 @@ picked_powerball = []
 
 def generate_winner(numbers):
 	"""Simulates winning lottery numbers (not including the powerball)"""
-	while len(winning_numbers) < 5:
+	while len(winning_numbers) < 3:
 		#	Randomly select number from list of possible numbers and assign it a variable
 		drawn_digit = choice(numbers)
 		#	Ensures that no numbers are repeated
@@ -49,7 +49,7 @@ def generate_winning_powerball(powerballs):
 
 def generate_ticket(numbers):
 	"""Simulates a purchased lottery ticket (not including the powerball)"""
-	while len(picked_numbers) < 5:
+	while len(picked_numbers) < 3:
 		#	Randomly select numbers from list of possible numbers and assign a variable
 		drawn_digit = choice(numbers)
 		#	Ensures that no numbers are repeated
@@ -62,16 +62,23 @@ def generate_ticket(numbers):
 def generate_powerball(powerballs, picked_numbers):
 	"""Simulates a purchased powerball number"""
 	while len(picked_powerball) < 1:
+		#	Randomly select number from list of possible powerball numbers and assign a variable.
 		select_powerball = choice(powerballs)
+		#	Ensures that no powerball numbers are repeated (this isnt entirely needed because only
+		#	one powerball is needed but this allows the class to be consistent with rest of code)
 		if select_powerball not in picked_numbers:
+			#	Adds the powerball digit to a list (if not a repeat)
 			picked_powerball.append(select_powerball)
+	#	Returns the picked powerball
 	return picked_powerball
 
 def check_ticket(picked_numbers, winning_numbers):
 	"""Checks the "purchased" lottery numbers against the winning numbers"""
 	for pick in picked_numbers:
+		#	If the "purchased" ticket numbers are not in the winning numbers, it returns false
 		if pick not in winning_numbers:
 			return False
+	#	If the "purchased" ticket numbers are in the winning numbers, it returns true
 	return True
 
 def check_powerball(picked_powerball, winning_powerball):
@@ -81,17 +88,30 @@ def check_powerball(picked_powerball, winning_powerball):
 			return False
 	return True
 
+#	Range of each non-powerball lottery pick is 1 to 69. 
 numbers = range(0, 70)
+
+#	Range of each powerball lottery pick is 1 to 26.
 powerballs = range(0, 27)
+
 winning_numbers = generate_winner(numbers)
 winning_powerball = generate_winning_powerball(powerballs)
 
+#	Ticket Counter which increases as each ticket is simulated and checked against the winning 
+#	numbers
 tickets = 0
+
+#	Default states for winning criteria are false until a "purchased" ticket matches the winning 
+#	combo
 won = False
 hit_powerball = False
 
-max_tries = 1_000_000_000
+#	Configurable break for the program to keep it from running too long. Note, the real Powerball 
+#	lottery has odds of 1 in 292,201,338.
+max_tries = 293_000_000
 
+#	Logic for generating a winner and then generating tickets to compare to incrementing if there
+#	is no winner. 
 while not won:
 	new_ticket = generate_ticket(numbers)
 	new_powerball = generate_powerball(powerballs, picked_numbers)
