473d86f334b4e8b2f0084b0425812936128cf685
mateospencer <matt@mattspencer.net>
2020-05-16 13:02:42 +1200

    exercise added for 10-1

---
+++ p191-files_exceptions.py
@@ -0,0 +1,10 @@
+#	Python Crash Course 2nd Edition
+#	Page 191 (Files and Exceptions)
+
+#	Section 10-1: Learning Python
+#	Open a blank file in your text editor and write a few lines summarizing what you've learned
+#	learned about Python so far. Start each line with the phrase In Python you can.... Save the file
+#	 as learning_python.txt in the same directory as your exercises from this chapter. Write a 
+#	program tha reads the file and prints what you wrote three times. Print the contents once by 
+#	reading in the entire file, once by looping over the file object, and once by storing the lines 
+#	in a list and then working with them outside the with block.
